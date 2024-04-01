da187d1169e71fa7f3e231cc489b4f99fd80bfc1
imtarun2001 <tarunkumarbehera8249@gmail.com>
2024-04-01 18:58:36 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+function OpenNav() {
+    document.getElementById("Navbar").style.width = "250px";
+}
+
+function CloseNav() {
+    document.getElementById("Navbar").style.width = "0px";
+}
+
+function slideShowrightwillcome() {
+    document.querySelector(".subcontent7aa").style.width = "0px"
+    document.querySelector(".subcontent7ab").style.width = "75%"
+}
+
+function slideShowleftwillcome() {
+    document.querySelector(".subcontent7ab").style.width = "0px"
+    document.querySelector(".subcontent7aa").style.width = "75%"
+}
